02dc57e55dfec30b4ac40e74e7da193da3fe24f1
drago-develops <ambro.rot@gmail.com>
2024-08-30 16:36:40 +0100

    on click functions to display digits
    for each of the number button I've added onclick function
    so that user can see number inputed on the display.
    easy way to keeping track of the calculation input.

---
+++ calculator.js
@@ -0,0 +1,30 @@
+function num7(){
+    document.getElementById('calculations').textContent += '7';
+}
+function num8(){
+    document.getElementById('calculations').textContent += '8';
+}
+function num9(){
+    document.getElementById('calculations').textContent += '9';
+}
+function num4(){
+    document.getElementById('calculations').textContent += '4';
+}
+function num5(){
+    document.getElementById('calculations').textContent += '5';
+}
+function num6(){
+    document.getElementById('calculations').textContent += '6';
+}
+function num3(){
+    document.getElementById('calculations').textContent += '3';
+}
+function num2(){
+    document.getElementById('calculations').textContent += '2';
+}
+function num1(){
+    document.getElementById('calculations').textContent += '1';
+}
+function num0(){
+    document.getElementById('calculations').textContent += '0';
+}
